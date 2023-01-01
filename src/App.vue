7808c83824202707365b84f5deddf0119531c18c
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-01 09:18:55 -0300

    refactoring routes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
-  <router-view></router-view>
+
+  <Header></Header>
+
+  <Home v-if="setPages === 'Home'"/>
+  <User v-if="setPages === 'User'"/>
+  <Repository v-if="setPages === 'Repository'"/>
+  <Favorites v-if="setPages === 'Favorites'"/>
+
 </template>
 
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Header from './components/Header.vue'
+import Home from './components/Home.vue'
+import User from './components/User.vue'
+import Repository from './components/Repository.vue'
+import Favorites from './components/Favorites.vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  data () {
+    return {
+      setPages: 'Home'
+    }
+  },
+
+  components: {
+    Header,
+    Home,
+    User,
+    Repository,
+    Favorites
+  },
+
 });
 </script>
 
 <style>
-body{
-  font-size: 25px;
+
+*{
+  margin: 0;
+  text-decoration: none;
+  font-family: 'Inter';
+  box-sizing: border-box;
 }
+
 
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/user',
-        name: 'User',
-        component: () => import('../views/User.vue')        
-    },
-    {
-        path: '/repository',
-        name: 'Repository',
-        component: () => import('../views/Repository.vue')        
-    },
-    {
-        path: '/favorites',
-        name: 'Favorites',
-        component: () => import('../views/Favorites.vue')        
-    },
-    {
-        path: '/:catchAll(.*)',
-        name: 'PageNotFound',
-        component: () => import('../views/PageNotFound.vue')        
-    },
-]
+// const routes = [
+//     {
+//         path: '/:catchAll(.*)',
+//         name: 'PageNotFound',
+//         component: () => import('../views/PageNotFound.vue')        
+//     },
+// ]
 
-const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
-})
+// const router = createRouter({
+//     history: createWebHistory(process.env.BASE_URL),
+//     routes
+// })
 
-export default router
+// export default router
