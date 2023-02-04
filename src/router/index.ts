4d7b654316a288b2e4e9bb2c9bf14c7cd46f3da3
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-02-04 20:55:25 -0300

    adding route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'Home',
