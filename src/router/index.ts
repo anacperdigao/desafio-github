82d257adf940d7c3c9f0a4451bffc6df948e503a
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-02-25 19:44:52 -0300

    feat: adding route 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,23 +12,16 @@ const routes: RouteRecordRaw[] = [
         name: 'UsersList',
         component: () => import('../views/UsersList.vue') 
     },
-
-    // {
-    //     path: '/user',
-    //     name: 'User',
-    //     component: () => import('../views/User.vue')        
-    // },
-    // {
-    //     path: '/repository',
-    //     name: 'Repository',
-    //     component: () => import('../views/Repository.vue')        
-    // },
-
-    // {
-    //     path: '/:catchAll(.*)',
-    //     name: 'PageNotFound',
-    //     component: () => import('../views/PageNotFound.vue')        
-    // },
+    {
+        path: '/user',
+        name: 'User',
+        component: () => import('../views/User.vue')        
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'PageNotFound',
+        component: () => import('../views/PageNotFound.vue')        
+    },
 ]
 
 const router = createRouter({
